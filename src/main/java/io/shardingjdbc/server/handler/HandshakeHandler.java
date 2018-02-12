90cd842e5e8fa3771f256413eb4bc20322f8f5b6
terrymanu <terrymanu@163.com>
2018-02-13 01:27:53 +0800

    fix sequence id for auth ok packet

--- src/main/java/io/shardingjdbc/server/handler/HandshakeHandler.java
+++ src/main/java/io/shardingjdbc/server/handler/HandshakeHandler.java
@@ -3,6 +3,7 @@ package io.shardingjdbc.server.handler;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.shardingjdbc.server.constant.StatusFlag;
 import io.shardingjdbc.server.packet.MySQLPacketPayload;
 import io.shardingjdbc.server.packet.handshake.AuthPluginData;
 import io.shardingjdbc.server.packet.handshake.ConnectionIdGenerator;
@@ -30,6 +31,6 @@ public class HandshakeHandler extends ChannelInboundHandlerAdapter {
         HandshakeResponse41Packet response41 = new HandshakeResponse41Packet().read(new MySQLPacketPayload((ByteBuf) message));
         // TODO use authPluginData to auth
         
-        context.writeAndFlush(new OKPacket(1, 0L, 0L, 0, 0, "connection success"));
+        context.writeAndFlush(new OKPacket(2, 0L, 0L, StatusFlag.SERVER_STATUS_AUTOCOMMIT.getValue(), 0, ""));
     }
 }
